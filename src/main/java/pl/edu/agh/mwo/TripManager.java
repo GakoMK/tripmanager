6ce1c4b9db5fc9243219c26ef9ca07fe2d187beb
Michał Kletowski <mkletowski@mig.lan>
2019-05-16 07:45:40 +0200

    class

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -24,5 +24,32 @@ public class TripManager {
 	public void remove(String name) {
 		tripList.remove(name);
 	}
-	
+
+	// Wyszukiwanie
+	public List<Trip> findTrip(String keyword){
+		List<Trip> results = new ArrayList<Trip>();
+        for (String tripName : tripList.keySet()) {
+			
+			// słowo kluczowe to pusty string
+            if (keyword.isEmpty()) {
+				results.add(tripList.get(tripName));
+			}
+			
+			// nazwa zawiera słowo kluczowe oraz opis zawiera słowo kluczowe
+        	if (tripName.toLowerCase().contains(keyword.toLowerCase()) || tripList.get(tripName).getDescription().toLowerCase().contains(keyword.toLowerCase())) {
+            	results.add(tripList.get(tripName));
+                return results;
+			}
+			
+			// komentarz jednego ze zdjęć obecnej w danej wycieczce zawiera słowo kluczowe
+        	List<Photo> photos = tripList.get(tripName).getPhotos();
+        	for (Photo photo : photos) {
+        		if (photo.getComment().contains(keyword)) {
+					results.add(tripList.get(tripName));
+				}
+			}
+        }
+		return results;
+	}
+
 }
